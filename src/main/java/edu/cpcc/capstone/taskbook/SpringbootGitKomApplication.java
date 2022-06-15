2c040f4981341e375333fef1d97c9784672c578d
Oksana-Kaz <nelkakaramel20@gmail.com>
2022-06-15 15:30:13 -0400

    initial release

---
+++ src/main/java/edu/cpcc/capstone/taskbook/SpringbootGitKomApplication.java
@@ -0,0 +1,13 @@
+package edu.cpcc.capstone.taskbook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootGitKomApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootGitKomApplication.class, args);
+	}
+
+}
---
+++ src/test/java/edu/cpcc/capstone/taskbook/SpringbootGitKomApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.cpcc.capstone.taskbook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootGitKomApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
